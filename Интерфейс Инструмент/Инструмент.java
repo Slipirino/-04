04b9ed44a7d0d803c496be0656e8c31fe1be6c54
Slipirino <124039026+Slipirino@users.noreply.github.com>
2023-02-15 23:39:21 +0300

    Delete Интерфейс Инструмент directory

--- Интерфейс Инструмент/Инструмент.java
+++
@@ -1,9 +0,0 @@
-public interface Инструмент {
-    String KEY = "До мажор";
-
-     void play() {
-
-    }
-
-
-}
--- Интерфейс Инструмент/Труба.java
+++
@@ -1,9 +0,0 @@
-public class Труба implements Инструмент{
-    int размер;
-    Труба( int size ) {
-        this.размер = размер;
-    }
-    public void play() {
-        System.out.println("Играет труба с размером "+ размер);
-    }
-}
